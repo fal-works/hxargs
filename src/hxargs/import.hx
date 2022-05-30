12775148dfd8ee2c4ede98d7bf9bc3c056a99f8c
FAL <falworks.contact@gmail.com>
2022-05-31 04:03:03 +0900

    refactor: remove unused module

--- src/hxargs/import.hx
+++ src/hxargs/import.hx
@@ -3,5 +3,4 @@ package hxargs;
 import hxargs.HxArgsError;
 import hxargs.internal.Maybe;
 
-using hxargs.internal.ArrayTools;
 using hxargs.internal.Iterables;
--- src/hxargs/internal/ArrayTools.hx
+++
@@ -1,4 +0,0 @@
-package hxargs.internal;
-
-inline function ifNonEmpty<T>(array: Array<T>, func: Array<T>->Void): Void
-	if (array.length > 0) func(array);
