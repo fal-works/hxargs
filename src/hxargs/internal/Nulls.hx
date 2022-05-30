1bf99b2e503c20f09d4d8063f595bbc6e109715a
FAL <falworks.contact@gmail.com>
2022-05-31 03:59:43 +0900

    improve merge()

--- src/hxargs/internal/Nulls.hx
+++ src/hxargs/internal/Nulls.hx
@@ -1,7 +1,5 @@
 package hxargs.internal;
 
-using Std;
-
 class Nulls {
 	@:noUsing
 	public static function merge<T>(
@@ -9,38 +7,34 @@ class Nulls {
 		b: Null<T>,
 		?propertyName: String
 	): Null<T> {
-		return if (a == null || a == b) b else if (b == null) a else {
-			if (a.isOfType(Bool)
-				|| a.isOfType(Int)
-				|| a.isOfType(Float)
-				|| a.isOfType(String)) {
-				throw new HxArgsConflictError(propertyName);
-			} else if (a.isOfType(Array)) {
-				cast mergePrimitiveArrays(
-					cast a,
-					cast b
-				); // Assuming there is no array of objects
-			} else {
-				final ret: Dynamic = {};
-				for (key in Reflect.fields(a)) {
-					Reflect.setField(ret, key, Reflect.field(a, key));
-				}
-				for (key in Reflect.fields(b)) {
-					final newValue = merge(
-						Reflect.field(ret, key),
-						Reflect.field(b, key),
-						key
-					);
-					if (newValue != null) Reflect.setField(ret, key, newValue);
-				}
-				ret;
-			}
+		return if (a == null || a == b) {
+			b;
+		} else if (b == null) {
+			a;
+		} else if (Std.isOfType(a, Array)) {
+			cast concatArrays(cast a, cast b);
+		} else switch Type.typeof(a) {
+			case TObject: mergeObjects(a, b);
+			case _: throw new HxArgsConflictError(propertyName);
 		}
 	}
 
-	static function mergePrimitiveArrays<T>(a: Array<T>, b: Array<T>): Array<T> {
-		final ret = a.copy();
-		for (be in b) if (!ret.exists(re -> re == be)) ret.push(be);
+	static function concatArrays<T>(a: Array<T>, b: Array<T>): Array<T>
+		return a.concat(b);
+
+	static function mergeObjects(a: Dynamic, b: Dynamic): Dynamic {
+		final ret: Dynamic = {};
+		for (key in Reflect.fields(a)) {
+			Reflect.setField(ret, key, Reflect.field(a, key));
+		}
+		for (key in Reflect.fields(b)) {
+			final newValue = merge(
+				Reflect.field(ret, key),
+				Reflect.field(b, key),
+				key
+			);
+			if (newValue != null) Reflect.setField(ret, key, newValue);
+		}
 		return ret;
 	}
 }
