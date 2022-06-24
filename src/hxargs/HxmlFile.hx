52fbd2b189065638c4f6578ad00576928c91eefb
FAL <falworks.contact@gmail.com>
2022-06-24 17:58:18 +0900

    improve HxmlFile class

--- src/hxargs/HxmlFile.hx
+++ src/hxargs/HxmlFile.hx
@@ -5,8 +5,24 @@ package hxargs;
 **/
 @:structInit
 class HxmlFile {
+	/**
+		Filepath to a corresponding `*.hxml` file.
+	**/
 	public final filepath: String;
 
+	/**
+		Invokes `haxe` command.
+
+		This is actually the same as `this.toCommand().execute()`.
+
+		@return Exit code.
+	**/
+	public function execute(): Int
+		return this.toCommand().execute();
+
+	/**
+		Converts `this` to a `haxe` command that can be executed on the CLI.
+	**/
 	public function toCommand(): Command {
 		return {
 			command: "haxe",
